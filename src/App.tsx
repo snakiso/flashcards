1404b1f7c7dccedbe6b793cdcbf6f89386091fec
snakiso <igorchagov@mail.ru>
2024-01-06 10:13:30 +0300

    git commit -m "feat: add components"

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox/Checkbox";
+import { Typography } from "@/components/ui/typography/Typography";
+
 export function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Typography as={"h1"} variant={"h1"}>
+        Hello
+      </Typography>
+      <Button as={"button"}>Some text</Button>
+      <Checkbox label={"hello"} />
+    </div>
+  );
 }
